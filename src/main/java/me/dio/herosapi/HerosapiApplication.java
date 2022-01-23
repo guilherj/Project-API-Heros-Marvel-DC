e6869c5026a9f92c93b6d6b21a5a3e875c049675
guilherj <contato.guilhermenascimento@hotmail.com>
2022-01-23 10:17:13 -0300

    Projeto Iniciado

---
+++ src/main/java/me/dio/herosapi/HerosapiApplication.java
@@ -0,0 +1,13 @@
+package me.dio.herosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerosapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerosapiApplication.class, args);
+	}
+
+}
